d95b9d5130e54ac85e6c06c2bce22a0ce798c73e
Maivu <maivanphuongvu.dhsp@gmail.com>
2025-07-23 09:46:25 +0700

    change FileSystem to Package

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99089133410d361b1ba6301dfe8d7858346e57d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+154604887b8e203ae4da10043ce2653f52232c5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

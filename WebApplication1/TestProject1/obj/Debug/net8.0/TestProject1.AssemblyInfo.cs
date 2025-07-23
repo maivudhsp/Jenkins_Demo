154604887b8e203ae4da10043ce2653f52232c5f
Maivu <maivanphuongvu.dhsp@gmail.com>
2025-07-23 09:02:49 +0700

    change publish

--- WebApplication1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ WebApplication1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0452af3141074eb544075f77bf8eae5c41624fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99089133410d361b1ba6301dfe8d7858346e57d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
